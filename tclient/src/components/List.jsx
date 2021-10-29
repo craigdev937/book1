eb2a917a4481b4d3fbb1da1a58fcc8130f631ecb
Craig <indigomx9@gmail.com>
2021-10-29 19:49:39 -0400

    Updaated the List.jsx component.

--- tclient/src/components/List.jsx
+++ tclient/src/components/List.jsx
@@ -1,11 +1,55 @@
 import React from "react";
-// Relates to his Student Page.
+import { Col, Card, Typography, message } from "";
+import { EditOutlined, DeleteOutlined, EyeOutlined } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
+import { TodoAPI } from "../global/TodoAPI";
 
-export const List = () => {
+export const List = ({ todo }) => {
+    const { Title, Paragraph } = Typography;
+    const history = useHistory();
+    const [deleteTodo, { isLoading, isSuccess }] = 
+        TodoAPI.useDeleteTodoMutation();
+
+    React.useEffect(() => {
+        if (isLoading) {
+            message.loading({ content: "deleting..." });
+        };
+        if (isSuccess) {
+            message.success({ 
+                content: "Todo was Deleted", duration: 2
+            });
+        };
+    }, [isLoading, isSuccess]);
+
     return (
         <React.Fragment>
-            <h1>List</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt illo aliquam iusto velit reprehenderit? Repudiandae, placeat. Nostrum quas assumenda fuga sit veritatis ea laudantium mollitia, consequuntur ratione, dicta commodi ullam.</p>
+            <Col span={6} key={id}>
+                <Card
+                    hoverable={true}
+                    bordered={false}
+                    actions={[
+                        <EyeOutlined 
+                            key="view"
+                            onClick={() => history.push(`/${id}`)}
+                        />,
+                        <EditOutlined 
+                            key="edit"
+                            onClick={() => history.push(`/edit/${id}`)}
+                        />,
+                        <DeleteOutlined 
+                            key="setting"  
+                            onClick={() => deleteTodo(id)}
+                        />,
+                    ]}>
+                        <main className="student-info">
+                            <Title level={5}>{todo.title}</Title>
+                            <Paragraph>{todo.author}</Paragraph>
+                            <Paragraph>{todo.info}</Paragraph>
+                            <Paragraph>{todo.amount}</Paragraph>
+                            <Paragraph>{todo.published}</Paragraph>
+                        </main>
+                </Card>
+            </Col>            
         </React.Fragment>
     );
 };
